8ba9987961718121b588ad197a6f0e19ae2e4673
Noorulain <nooryesheikh45@gmail.com>
2026-03-23 16:58:21 +0500

    comit

--- chat-app/src/lib/socket.ts
+++ chat-app/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "https://sockit-io-library.vercel.app";
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "  http://localhost:3000";
 
 let socket: Socket | null = null;
 
